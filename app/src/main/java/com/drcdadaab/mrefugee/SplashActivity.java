55a38fb824da0ef5db77a55d6946bfc495c44e61
Oriaso <micahoriaso@gmail.com>
2017-09-11 17:00:20 +0300

    Implemented splash screen

--- app/src/main/java/com/drcdadaab/mrefugee/SplashActivity.java
+++ app/src/main/java/com/drcdadaab/mrefugee/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.drcdadaab.mrefugee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,5 +9,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
